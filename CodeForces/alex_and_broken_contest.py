c07ce445168c6c5ccdcda5ca01185198b53bedb8
nicholasting92 <nicholasting92@gmail.com>
2020-04-22 13:49:19 +1000

    Solved

--- CodeForces/alex_and_broken_contest.py
+++ CodeForces/alex_and_broken_contest.py
@@ -9,32 +9,17 @@ n = "Nikita"
 
 friends = [d,o,s,a,n]
 
-repeat_friends = False
-no_friends = 0
 unique_friends = 0
-check = check.lower()
 
 for friend in friends:
 
-    friend = friend.lower()
-    test = check.split(friend)
-    if test[0] == check:
-        no_friends += 1
+    test = check.count(friend)
+    unique_friends += test
+    # print(test)
+    # if len(test) == 2:
+    #     unique_friends += 1
 
-    if len(test) == 2:
-        unique_friends += 1
-    
-    if len(test) > 3:
-        repeat_friends = True
-        break
-    
-if repeat_friends:
-    print("NO")
-
-elif no_friends == 5:
-    print("NO") 
-
-elif unique_friends == 1:
+if unique_friends == 1:
     print("YES")
 
 else:
