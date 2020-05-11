db3d64577856147c261fb151eff4f4d357fc94b5
nicholasting92 <nicholasting92@gmail.com>
2020-05-11 15:38:43 +1000

    upsolve D

---
+++ CodeForces_640/d.py
@@ -0,0 +1,17 @@
+T = int(input())
+
+while T != 0:
+    N = int(input())
+    nums = list(map(int,input().split()))
+    first = nums[0]
+    
+    nums = nums[1:]
+    alice = 0
+    bob = 0
+    prev_count = 1
+    while len(nums) != []:
+        
+        pass
+
+
+    T -= 1
